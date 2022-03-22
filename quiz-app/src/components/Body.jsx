29550f967c7021a8b7b54d046ed4548a288b8d77
Oluwaseyi-Java <adeseyitu@gmail.com>
2022-03-22 23:22:10 +0100

    Home page

---
+++ quiz-app/src/components/Body.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import BodyCard from './BodyCard'
+
+const Body = () => {
+  return (
+    <div className='body'>
+        <BodyCard />
+          <BodyCard />
+          <BodyCard />
+          <BodyCard />
+          <BodyCard />
+          <BodyCard />
+          <BodyCard />
+        <BodyCard />
+    </div>
+  )
+}
+
+export default Body
---
+++ quiz-app/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <div className='project-name'>
+        QuizApp
+      </div>
+    </div>
+  )
+}
+
+export default Header
